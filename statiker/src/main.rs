9cafc62ff8835fae6317269077185da2453cf43d
padi2312 <patrick.arndt.99@web.de>
2024-03-08 01:07:59 +0100

    file upload endpoint + designed upload page

--- statiker/src/main.rs
+++ statiker/src/main.rs
@@ -2,33 +2,41 @@ use std::collections::HashMap;
 
 use config::parse_arguments;
 use surfer::{
-    request::{Method::GET, Request},
+    request::{Method::POST, Request},
     response::Response,
     route,
     server::Server,
 };
-
 pub mod config;
 
-async fn index(request: Request) -> Response {
-    println!("{:#?}", request);
-    let mut headers: HashMap<String, String> = HashMap::new();
-    headers.insert("Content-Type".to_string(), "text/html".to_string());
-    headers.insert("Server".to_string(), "Statiker".to_string());
+async fn upload(request: Request, save_path: String) -> Response {
+    let files = request.files;
+    for (file_name, file_content) in files {
+        async_std::fs::write(format!("{}/{}", save_path, file_name), file_content)
+            .await
+            .unwrap();
+    }
 
-    Response::new(200, headers, b"Hello, World!".to_vec())
+    let mut headers: HashMap<String, String> = HashMap::new();
+    let response_data = b"{\"success\": true}".to_vec();
+    headers.insert("Content-Type".to_string(), "application/json".to_string());
+    headers.insert(
+        "Content-Length".to_string(),
+        response_data.len().to_string(),
+    );
+    headers.insert("Server".to_string(), "Statiker".to_string());
+    Response::new(200, headers, response_data)
 }
 
 #[async_std::main]
 async fn main() {
     let config = parse_arguments();
-    let mut server = Server::new(
-        config.address.to_string(),
-        config.port.to_string(),
-    );
+    let mut server = Server::new(config.address.to_string(), config.port.to_string());
     server
-        .register_static_dir("/data", Some(config.root_dir.to_str().unwrap_or(".")))
+        .register_static_dir("/", Some(config.root_dir.to_str().unwrap_or(".")))
         .await;
-    server.register_route(route!(GET, "/", index));
+
+    let static_dir = config.root_dir.to_str().unwrap_or(".").to_string();
+    server.register_route(route!(POST, "/upload", upload, static_dir));
     server.listen().await;
 }
